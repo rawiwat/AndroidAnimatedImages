242aec6f6ed237ff00e380146b83bc53d99c79b2
Peter <rawiwatpan@gmail.com>
2022-12-29 17:44:17 +0700

    second and third gif

--- animationimage/app/src/main/java/com/example/animationimage/MainActivity.kt
+++ animationimage/app/src/main/java/com/example/animationimage/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.animationimage
 
 import android.annotation.SuppressLint
 import android.graphics.ImageDecoder
+import android.graphics.drawable.Animatable
 import android.graphics.drawable.AnimatedImageDrawable
-import android.media.Image
+import android.graphics.drawable.AnimationDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +16,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
+        this add and animate a gif
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             val gifDrawable = ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, R.drawable.amogus))
             animated_image.setImageDrawable(gifDrawable)
             (gifDrawable as AnimatedImageDrawable).start()
         }
-        animated_image
+        this add gif with animated drawable
+        val frameDrawable = ContextCompat.getDrawable(this,R.drawable.bonk_animated)
+        animated_image.setImageDrawable(frameDrawable)
+        (frameDrawable as AnimationDrawable).start()
+        this use vector drawable
+        */
+        val animatedVectorDrawable = ContextCompat.getDrawable(this,R.drawable.avd_playtopause)
+        animated_image.setImageDrawable(animatedVectorDrawable)
+        (animatedVectorDrawable as Animatable).start()
     }
 }
